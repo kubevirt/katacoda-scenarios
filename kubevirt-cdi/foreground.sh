86de645c1e09ce90f02f309d30bc23e74639cd01
cwilkers <cwilkers@redhat.com>
2022-07-13 19:08:59 -0500

    Unify scenario descriptions
    Signed-off-by: cwilkers <cwilkers@redhat.com>

--- kubevirt-cdi/foreground.sh
+++ kubevirt-cdi/foreground.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+until kubectl -n kubevirt wait --for=jsonpath='{.status.phase}'=Deployed kubevirt/kubevirt --timeout 9m
+do
+    sleep 30
+done
+
 export PS1="\[\e[1;33m\]\h $ \[\e[1;36m\]"
 trap 'echo -ne "\e[0m"' DEBUG
 
-sleep 20
-kubectl -n kubevirt wait --for=jsonpath='{.status.phase}'=Deployed kubevirt/kubevirt --timeout 9m && echo "KubeVirt is deployed"
+echo "KubeVirt is deployed"
