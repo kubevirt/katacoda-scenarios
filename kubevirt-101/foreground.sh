3015b66eb0b78646f442fb129df2e88181e5be44
cwilkers <cwilkers@redhat.com>
2022-07-28 17:16:53 -0500

    Updates needed for base ubuntu env
    Signed-off-by: cwilkers <cwilkers@redhat.com>

--- kubevirt-101/foreground.sh
+++ kubevirt-101/foreground.sh
@@ -1,3 +1,5 @@
+alias k=kubectl
+
 export PS1="\[\e[1;33m\]\h $ \[\e[1;36m\]"
 trap 'echo -ne "\e[0m"' DEBUG
 echo
--- kubevirt-101/setup.sh
+++ kubevirt-101/setup.sh
@@ -1,1 +1,5 @@
+apt-get install -y jq
+
 curl -sfL https://get.k3s.io | sh -
+
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
