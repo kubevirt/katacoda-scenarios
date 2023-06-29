a4cd6cd5205e152c03fba671c397f776a8eb2462
hindu-muppala <hindumuppala@gmail.com>
2023-06-29 18:56:53 +0530

    Configured KubeVirt and Virctl

--- kubevirt-upgrades/background.sh
+++ kubevirt-upgrades/background.sh
@@ -1,18 +1,8 @@
-# #!/bin/bash
-
-# apt-get install -y jq
-
-# curl -sfL https://get.k3s.io | sh -
-
-# mkdir -m 750 ~/.kube
-# cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-
-
 #!/bin/bash
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-export KUBEVIRT_VERSION=v0.56.0
+export KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest | jq -r .tag_name)
 echo Installing KubeVirt $KUBEVIRT_VERSION
 
 kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-operator.yaml
