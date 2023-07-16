1273bcea1b888a9512671868fbefde86451db3d8
hindu-muppala <hindumuppala@gmail.com>
2023-07-16 16:04:13 +0530

    virtctl

--- kubevirt-upgrades/background.sh
+++ kubevirt-upgrades/background.sh
@@ -15,7 +15,7 @@ kubectl -n kubevirt patch kubevirt/kubevirt --type=merge --patch='{"spec": {"inf
 
 curl -sLo virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/virtctl-${KUBEVIRT_VERSION}-linux-amd64
 
-sudo install -m 0755  virtctl /usr/local/bin/virtctl
+sudo install -m 0775  virtctl /usr/local/bin/virtctl
 rm -f virtctl
 
 echo "StrictHostKeyChecking=no" > ${HOME}/.ssh/config
